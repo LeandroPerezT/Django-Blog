33c8bdafece75b093cf2de3a03f9bfa5a50ffd66
Leandro Perez Torres <leandropereztorres98@gmai..com>
2020-12-19 23:11:59 -0300

    commit on main

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 from posts.views import index, blog, post, search
 
 urlpatterns = [
@@ -11,7 +10,7 @@ urlpatterns = [
     path('blog/', blog, name='post-list'),
     path('search/', search, name = 'search'),
     path('post/<id>', post, name='post-detail'),
-
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 if settings.DEBUG:
